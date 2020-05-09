692fbcda690666e5108d3fc043c54394d581152d
Ivan <ivan.pmartell@hotmail.com>
2020-05-08 20:32:40 -0700

    Removing jekyll

--- docs/conf.py
+++ docs/conf.py
@@ -33,12 +33,12 @@ release = '0.0.1'
 extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
